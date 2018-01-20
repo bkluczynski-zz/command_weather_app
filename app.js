e67091a05afa80a962a7b803d67ac1eb39ceeea3
Bart <bart@paybase.io>
2018-01-20 13:12:26 +0000

    encoding address to URI

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const { argv } = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help();
+const { address } = argv;
+const encodedAddress = encodeURIComponent(address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=12+Sandringham+Rd,+London+E8+2LR&key=AIzaSyBe9ykA30J-jH9cRpt6G1xKOjj4nqQmomk',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBe9ykA30J-jH9cRpt6G1xKOjj4nqQmomk`,
   json: true,
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`${body.results[0].formatted_address}`);
 });
