9e4e6c133620cc1a0362a628d09135ba99d282a4
Bart <bart@paybase.io>
2018-01-20 13:35:11 +0000

    added errors handling

--- app.js
+++ app.js
@@ -18,5 +18,11 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBe9ykA30J-jH9cRpt6G1xKOjj4nqQmomk`,
   json: true,
 }, (error, response, body) => {
-  console.log(`${body.results[0].formatted_address}`);
+  if (error) {
+    console.log('Google services unavailable. Try again later.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Try different address.');
+  } else if (body.status === 'OK') {
+    console.log(`${body.results[0].formatted_address}`);
+  }
 });
