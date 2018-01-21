64519d78b19355d7242dd5be42b32dd021d6dfbc
Bart <bart@paybase.io>
2018-01-21 13:59:24 +0000

    refactored callbacks into Promises, wrapped request in promise object

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+
 const { argv } = yargs
   .options({
     a: {
@@ -11,23 +12,17 @@ const { argv } = yargs
   })
   .help();
 
-geocode.getAddress(argv.address, (error, results) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-    const { latitude } = results;
-    const { longitude } = results;
-    if (latitude && longitude) {
-      geocode.getTemperature(latitude, longitude, (err, temperature) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.stringify(temperature, undefined, 2));
-        }
+geocode.getAddress(argv.address)
+  .then((results) => {
+    geocode.getTemperature(results.latitude, results.longitude)
+      .then((temp) => {
+        console.log(`Current temperature is ${temp.weather}`);
+      })
+      .catch((errorMessage) => {
+        console.log(errorMessage);
       });
-    } else {
-      console.log('correct your params');
-    }
-  }
-});
+    console.log(`Your address is ${results.address}`);
+  })
+  .catch((errorMessage) => {
+    console.log('Something went wrong, ', errorMessage);
+  });
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,40 +1,43 @@
 const request = require('request');
 
-const getTemperature = (lat, lng, callback) => {
-  request({
-    url: `https://api.darksky.net/forecast/e8ffa8a65d4e06fa7cb561c3231d9beb/${lat},${lng}`,
-    json: true,
-  }, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      callback(undefined, {
-        weather: body.currently.temperature,
-      });
-    } else {
-      callback('Something went wrong', error);
-    }
+const getTemperature = (lat, lng) =>
+  new Promise((resolve, reject) => {
+    request({
+      url: `https://api.darksky.net/forecast/e8ffa8a65d4e06fa7cb561c3231d9beb/${lat},${lng}`,
+      json: true,
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve({
+          weather: body.currently.temperature,
+          apparentTemperature: body.currently.apparentTemperature,
+        });
+      } else {
+        reject(new Error('Something went wrong'));
+      }
+    });
   });
-};
 
 
-const getAddress = (address, callback) => {
-  const encodedAddress = encodeURIComponent(address);
-  request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBe9ykA30J-jH9cRpt6G1xKOjj4nqQmomk`,
-    json: true,
-  }, (error, response, body) => {
-    if (error) {
-      callback('Google services unavailable. Try again later.');
-    } else if (body.status === 'ZERO_RESULTS') {
-      callback('Try different address.');
-    } else if (body.status === 'OK') {
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        latitude: body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng,
-      });
-    }
+const getAddress = address =>
+  new Promise((resolve, reject) => {
+    const encodedAddress = encodeURIComponent(address);
+    request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBe9ykA30J-jH9cRpt6G1xKOjj4nqQmomk`,
+      json: true,
+    }, (error, response, body) => {
+      if (error) {
+        reject(new Error('Google services unavailable. Try again later.'));
+      } else if (body.status === 'ZERO_RESULTS') {
+        reject(new Error('Try different address.'));
+      } else if (body.status === 'OK') {
+        resolve({
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng,
+        });
+      }
+    });
   });
-};
 
 module.exports = {
   getAddress,
